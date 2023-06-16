d9bd7dfc3d078525177ba831bb0e629cd2483aee
OracleOfDarkness <121413528+OracleOfDarkness@users.noreply.github.com>
2023-06-16 15:29:51 -0700

    Version 0.024 Started Loot Tables Added Motar and pestle Recipe added Mana Bowl recipe Added New textures for manas started the Spellpages

--- src/main/java/net/mcreator/arcaneheart/item/BowlOfManaItem.java
+++ src/main/java/net/mcreator/arcaneheart/item/BowlOfManaItem.java
@@ -2,10 +2,23 @@
 package net.mcreator.arcaneheart.item;
 
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+
+import net.mcreator.arcaneheart.init.ArcaneHeartModItems;
 
 public class BowlOfManaItem extends Item {
 	public BowlOfManaItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem() {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		return new ItemStack(ArcaneHeartModItems.MAGIC_BOWL.get());
 	}
 }
--- src/main/java/net/mcreator/arcaneheart/item/MortarPestleItem.java
+++ src/main/java/net/mcreator/arcaneheart/item/MortarPestleItem.java
@@ -2,10 +2,31 @@
 package net.mcreator.arcaneheart.item;
 
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 
 public class MortarPestleItem extends Item {
 	public MortarPestleItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+		super(new Item.Properties().durability(150).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public boolean hasCraftingRemainingItem() {
+		return true;
+	}
+
+	@Override
+	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
+		ItemStack retval = new ItemStack(this);
+		retval.setDamageValue(itemstack.getDamageValue() + 1);
+		if (retval.getDamageValue() >= retval.getMaxDamage()) {
+			return ItemStack.EMPTY;
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isRepairable(ItemStack itemstack) {
+		return false;
 	}
 }
